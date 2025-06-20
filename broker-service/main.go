518c6a27b0d37d2b681ddd83eaa65ab2bbfc75e4
KittyChen913 <nightmare.kitty111@gmail.com>
2025-06-20 17:38:53 +0800

    [broker-service] add cors setting

--- broker-service/main.go
+++ broker-service/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"broker-service/routes"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	server := gin.Default()
 
+	server.Use(cors.Default())
 	routes.RegisterRoutes(server)
 	server.Run()
 }
