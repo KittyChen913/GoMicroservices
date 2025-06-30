8d961b912762bb845071d51da4bd58752670a85e
KittyChen913 <nightmare.kitty111@gmail.com>
2025-06-30 14:46:09 +0800

    [authentication-service] postgres db add retry mechanism

--- authentication-service/db/postgres.go
+++ authentication-service/db/postgres.go
@@ -3,12 +3,14 @@ package db
 import (
 	"context"
 	"os"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	_ "github.com/joho/godotenv/autoload"
 )
 
 var DbPool *pgxpool.Pool
+var retryCount int = 3
 
 func InitDb() {
 	var err error
@@ -16,11 +18,28 @@ func InitDb() {
 
 	config, err := pgxpool.ParseConfig(connStr)
 	if err != nil {
-		panic("Unable to connect to the database.")
+		panic("Unable to parse database config: " + err.Error())
 	}
 	config.MaxConns = 10
 	config.MinConns = 5
 
 	// 建立連線池
 	DbPool, err = pgxpool.NewWithConfig(context.Background(), config)
+	if err != nil {
+		panic("Unable to connect to the database: " + err.Error())
+	}
+
+	// 連線測試
+	for i := 1; i <= retryCount; i++ {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		err = DbPool.Ping(ctx)
+		cancel()
+		if err == nil {
+			break
+		}
+		if i == retryCount {
+			panic("Database ping failed: " + err.Error())
+		}
+		time.Sleep(2 * time.Second)
+	}
 }
